bca5b7099aee1fa1c52193c6055f968b1e87eed0
Giridhar Gedela <giridhargedela2908@gmail.com>
2026-07-18 13:03:49 +0530

    Add Jam-together project

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // This allows external connections
+//     port: 5173,      // Explicitly set the port
+//   }
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+})
